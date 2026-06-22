1a1a019a311e09d9d4f8067ea636040dd1ee6977
Dillersalda <dillerestudos@gmail.com>
2026-06-22 07:18:20 -0300

    introduzindo mensagens de erro no cadastro, e debuggando meu primeiro codigo

--- padaria/components/Javascript/modal.js
+++ padaria/components/Javascript/modal.js
@@ -1,6 +1,12 @@
 const btnCadastro = document.getElementById('btn-cadastro');
 const modal = document.getElementById('modal-cadastro');
 const fechar = document.getElementById('fechar-modal');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const btnCriarConta = document.querySelector('.btn-criar-conta');
+const mensagemErro = document.getElementById('mensagem-erro');
+
 
 btnCadastro.addEventListener('click',function(){
     modal.classList.add('ativo')
@@ -8,4 +14,24 @@ btnCadastro.addEventListener('click',function(){
 
 fechar.addEventListener('click', function(){
     modal.classList.remove('ativo');
-})
+});
+
+btnCriarConta.addEventListener('click', () => {
+
+    if(nome.value === ""){
+        mensagemErro.textContent = 'Insira um nome.';
+        return;
+    }
+
+    if(email.value === ""){
+        mensagemErro.textContent = 'insira um email.';
+        return;
+    }
+
+    if(senha.value === ""){
+        mensagemErro.textContent = 'Insira uma senha';
+        return;
+    }
+
+    mensagemErro.textContent = 'Conta Criada com Sucesso';
+}); 
